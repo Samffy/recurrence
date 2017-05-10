f3e4ed5b6ba2f85a51c1f9eb1808f93e2277da5e
Samuel <samuel.jobard@gmail.com>
2017-05-10 22:34:58 +0200

    Benchmark multiple periods to smooth results

--- benchmark.php
+++ benchmark.php
@@ -1,74 +1,50 @@
 <?php
 
-$statistics = [];
+define('ITERATION', 40);
+define('INCREMENT', 1);
 
 require 'vendor/autoload.php';
+require 'src/Benchmark.php';
 
-$start = new \Datetime(sprintf('%s-%s-01', rand(1000, 9999), rand(1, 12)));
-$end   = clone $start;
-$end->modify('+4 year'); // When you adding greater period, PHP native superiority is less important
+// Init statistics values
+$statistics = [];
+$statistics['recurr']['time']     = 0;
+$statistics['php']['time']        = 0;
+$statistics['recurrence']['time'] = 0;
 
-echo sprintf("Period : %s to %s\n\n", $start->format('c'), $end->format('c'));
+$benchmark = new Benchmark();
 
-//----------------------------------
-// Recurr library
+$iteration = 1;
+$years     = 1;
+while($iteration <= ITERATION) {
+    echo '.';
+    // Init new period with new interval in years
+    $start = new \Datetime(sprintf('%s-%s-01', rand(1000, 9999), rand(1, 12)));
+    $end   = clone $start;
+    $end->modify(sprintf('+%s year', $years));
 
-$startAt = microtime(true);
+    $statistics['recurr']['time']     += $benchmark->launchRecurrGeneration($start, $end);
+    $statistics['recurrence']['time'] += $benchmark->launchRecurrenceGeneration($start, $end);
+    $statistics['php']['time']        += $benchmark->launchNativePhpGeneration($start, $end);
 
-$rule = (new \Recurr\Rule)
-    ->setStartDate($start)
-    ->setUntil($end)
-    ->setFreq('MONTHLY')
-;
-$transformer = new \Recurr\Transformer\ArrayTransformer();
-$recurrences = $transformer->transform($rule);
-
-foreach ($recurrences as $recurrence) {
-    $recurrence->getStart()->format('c')."\n";
+    $years += INCREMENT;
+    $iteration++;
 }
 
-$statistics['recurr']['time'] = microtime(true) - $startAt;
+echo "\n\n";
 
-//----------------------------------
-// PHP native
+echo "# Parameters\n";
 
-$startAt = microtime(true);
+echo sprintf(" Iterations: %s\n", ITERATION);
+echo sprintf(" Increment testing period from [1] to [%s] years by step of [%s]\n", INCREMENT*ITERATION, INCREMENT);
 
-$interval = new \DateInterval('P1M');
-$period = new \DatePeriod($start, $interval, $end);
+echo "\n";
 
-// Need to process results, without that, PHP native is too damn fast ! Seems results are generated when accessing data
-foreach ($period as $date) {
-    $date->format('c')."\n";
-}
+echo "# Average time\n";
 
-$statistics['php']['time'] = microtime(true) - $startAt;
-
-//----------------------------------
-// New recurrence library
-
-$startAt = microtime(true);
-
-$recurrence = (new \Recurrence\Recurrence())
-    ->setPeriodStartAt($start)
-    ->setPeriodEndAt($end)
-    ->setFrequency(new \Recurrence\Frequency(\Recurrence\Frequency::FREQUENCY_MONTHLY))
-;
-$period = (new \Recurrence\DatetimeProvider($recurrence))->provide();
-
-foreach ($period as $date) {
-    $date->format('c')."\n";
-}
-
-$statistics['recurrence']['time'] = microtime(true) - $startAt;
-
-//----------------------------------
-
-echo "# Time\n";
-
-echo sprintf(" PHP native : %s\n", round($statistics['php']['time'], 10));
-echo sprintf(" Recurr : %s\n", round($statistics['recurr']['time'], 10));
-echo sprintf(" Recurrence : %s\n", round($statistics['recurrence']['time'], 10));
+echo sprintf(" PHP native : %s\n", round($statistics['php']['time']/ITERATION, 10));
+echo sprintf(" Recurr : %s\n", round($statistics['recurr']['time']/ITERATION, 10));
+echo sprintf(" Recurrence : %s\n", round($statistics['recurrence']['time']/ITERATION, 10));
 
 echo "\n";
 
@@ -76,3 +52,5 @@ echo "# Compare\n";
 
 echo sprintf(" PHP native is x%s faster than Recurr\n", round($statistics['recurr']['time']/$statistics['php']['time'], 2));
 echo sprintf(" Recurrence is x%s faster than Recurr\n", round($statistics['recurr']['time']/$statistics['recurrence']['time'], 2));
+
+echo "\n";
