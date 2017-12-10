55c16d9f40358a28058e7b9fb0000722c6be46c7
Samuel Jobard <samuel.jobard@gmail.com>
2017-12-11 00:01:42 +0100

    Fix Insight report : Object parameters should be type hinted

--- src/Recurrence/DatetimeProvider.php
+++ src/Recurrence/DatetimeProvider.php
@@ -55,7 +55,7 @@ class DatetimeProvider
      * @param \Datetime  $datetime
      * @return \Datetime
      */
-    private function applyConstraints($recurrence, $datetime)
+    private function applyConstraints(Recurrence $recurrence, \DateTime $datetime)
     {
         $filteredDatetime = $datetime;
 
